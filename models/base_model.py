9fc07d80a810ffef32be2e564c39f7d6bc11d3b7
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2023-12-06 16:00:35 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """string representation"""
@@ -33,7 +33,7 @@ class BaseModel:
     def save(self):
         """save class"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """return dict of object"""
