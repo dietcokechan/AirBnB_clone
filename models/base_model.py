81169bb70c5cad3bd83d1acd9555e470c55c7214
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2023-12-06 14:57:58 +0200

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -18,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """string representation"""
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """save class"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """return dict of object"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""serializes instances to a JSON file/deserializes JSON file to instances"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage(BaseModel):
+    """definition"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns dictionary of objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """adds new object to objects"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serialize objects to json file"""
+        jsonData = {}
+        for key, value, in self.__objects.items():
+            jsonData[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(jsonData, f)
+
+    def reload(self):
+        """deserialize json file to objects"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+                for key, obj in data.items():
+                    new = eval(obj['__class__'])(**obj)
+                    self.__objects[key] = new
+        except FileNotFoundError:
+            pass
