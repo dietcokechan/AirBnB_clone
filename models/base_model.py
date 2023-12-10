11428d50f8609f7f89dba1723e77d7fcfd084c6d
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2023-12-10 20:56:45 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -19,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """string representation"""
@@ -33,7 +33,7 @@ class BaseModel:
     def save(self):
         """save class"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """return dict of object"""
