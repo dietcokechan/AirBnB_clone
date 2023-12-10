10a7b0284a8ccf97fe05fa211de507c024a2893f
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2023-12-10 23:48:19 +0200

    default  function

--- console.py
+++ console.py
@@ -4,6 +4,13 @@ import cmd
 import shlex
 from shlex import split
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,22 +29,6 @@ class HBNBCommand(cmd.Cmd):
         """do nothing"""
         pass
 
-    def do_count(self, line):
-        """get count of instances of given class"""
-        strings = list(shlex.shlex(line, posix=True))
-        if strings[0] not in HBNBCommand.classes:
-            print("*** Unknown syntax: {}".format(line))
-            return
-        if strings[2] == "all":
-            self.do_all(strings[0])
-        elif strings[2] == "count":
-            count = 0
-            for obj in storage.all().values():
-                if strings[0] == type(obj).__name__:
-                    count += 1
-            print(count)
-            return
-
     def do_create(self, line):
         """creates an object"""
         if not len(line):
@@ -49,7 +40,6 @@ class HBNBCommand(cmd.Cmd):
         obj = eval(line)()
         print(obj.id)
         obj.save()
-        storage.save()
 
     def do_show(self, line):
         """shows an object"""
@@ -130,6 +120,60 @@ class HBNBCommand(cmd.Cmd):
         except Exception:
             setattr(storage.all()[keyVal], strings[2], strings[3])
         storage.save()
+
+    def strip_line(self, str):
+        """strip line"""
+        newstr = str[str.find("(")+1:str.rfind(")")]
+        newstr = shlex.shlex(newstr, posix=True)
+        newstr.whitespace += ','
+        newstr.whitespace_split = True
+        return list(newstr)
+
+    def strip_dict(self, str):
+        """try to find a dict while stripping line"""
+        newstr = str[str.find("(")+1:str.rfind(")")]
+        try:
+            newdict = newstr[newstr.find("{")+1:newstr.rfind("}")]
+            return eval("{" + newdict + "}")
+        except:
+            return None
+
+    def default(self, line):
+        """defaults"""
+        subargs = self.strip_line(line)
+        strings = list(shlex.shlex(line, posix=True))
+        if strings[0] not in HBNBCommand.classes:
+            print("*** Unknown syntax: {}".format(line))
+            return
+        if strings[2] == "all":
+            self.do_all(strings[0])
+        elif strings[2] == "count":
+            count = 0
+            for obj in storage.all().values():
+                if strings[0] == type(obj).__name__:
+                    count += 1
+            print(count)
+            return
+        elif strings[2] == "show":
+            key = strings[0] + " " + subargs[0]
+            self.do_show(key)
+        elif strings[2] == "destroy":
+            key = strings[0] + " " + subargs[0]
+            self.do_destroy(key)
+        elif strings[2] == "update":
+            newdict = self.dict_strip(line)
+            if type(newdict) is dict:
+                for key, val in newdict.items():
+                    keyVal = strings[0] + " " + subargs[0]
+                    self.do_update(keyVal + ' "{}" "{}"'.format(key, val))
+            else:
+                key = strings[0]
+                for arg in subargs:
+                    key = key + " " + '"{}"'.format(arg)
+                self.do_update(key)
+        else:
+            print("*** Unknown syntax: {}".format(line))
+            return
 
 
 if __name__ == '__main__':
